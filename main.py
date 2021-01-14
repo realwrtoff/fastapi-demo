c33acdeda806ac6752d931b2911fe407765c7478
realwrtoff <fmj_912@163.com>
2021-01-14 22:34:36 +0800

    add es

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from config.setting import settings
 from basic.errors import http_error_handler, http422_error_handler
 from basic.logger import logger
-from db.database import connect_mongo
+from db.database import connect_mongo, connect_es
 from api import router as api_router
 
 app = FastAPI(docs_url=settings.DOCS_URL, openapi_url=settings.OPENAPI_URL, redoc_url=settings.REDOC_URL)
@@ -33,12 +33,14 @@ app.add_middleware(
 @app.on_event('startup')
 async def on_startup() -> None:
     app.state.mgo_client, app.state.mgo_collections = await connect_mongo()
+    app.state.es = await connect_es()
     logger.info('连接数据库成功...')
 
 
 @app.on_event('shutdown')
 async def on_shutdown() -> None:
     app.state.mgo_client.close()
+    app.state.es.close()
     logger.info('关闭数据库, app退出')
 
 
@@ -48,4 +50,4 @@ app.add_exception_handler(RequestValidationError, http422_error_handler)
 app.include_router(api_router, prefix=settings.API_PREFIX)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host='0.0.0.0', port=settings.APP_PORT, reload=True, workers=settings.APP_WORKERS)
+    uvicorn.run("main:app", host='0.0.0.0', port=settings.APP_PORT, workers=settings.APP_WORKERS)
